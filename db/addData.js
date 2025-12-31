071649a068b54384e2598a73b01656954608687f
Keshav <keshavkhandelwal30@gmail.com>
2025-12-31 10:36:28 +0530

    Paused API Calls

--- db/addData.js
+++ db/addData.js
@@ -11,32 +11,34 @@ const main = async () => {
         const date = new Date();
         const year = date.getFullYear();
         console.log(`Adding Data ${date}`);
-        let data = [];
+        
+        // Commented to stop auto api calls
+        // let data = [];
         // for (body of carBodies) {
-        for (const body of carBodies) {
-            let bodyMakeModels = await fetchCarModel(body, year);
-            // for (cmm of bodyMakeModels) {
-            for (const cmm of bodyMakeModels.slice(0, 2)) {
-                const mm = cmm.make + " " + cmm.model;
-                const interest = await fetchModelInterest(mm);
-                data.push({
-                    make: cmm.make,
-                    model: cmm.model,
-                    bodyType: body.toLowerCase(),
-                    interestScore: interest,
-                });
-            }
-        }
+        // for (const body of carBodies) {
+        //     let bodyMakeModels = await fetchCarModel(body, year);
+        //     // for (cmm of bodyMakeModels) {
+        //     for (const cmm of bodyMakeModels.slice(0, 2)) {
+        //         const mm = cmm.make + " " + cmm.model;
+        //         const interest = await fetchModelInterest(mm);
+        //         data.push({
+        //             make: cmm.make,
+        //             model: cmm.model,
+        //             bodyType: body.toLowerCase(),
+        //             interestScore: interest,
+        //         });
+        //     }
+        // }
 
-        console.log(data);
-        if (data.length > 0) {
-            console.log("Old data deleted");
-            await Car.deleteMany({});
-            await Car.insertMany(data);
-            console.log(`Data added ${date}`);
-        } else {
-            console.log("No data added");
-        }
+        // console.log(data);
+        // if (data.length > 0) {
+        //     console.log("Old data deleted");
+        //     await Car.deleteMany({});
+        //     await Car.insertMany(data);
+        //     console.log(`Data added ${date}`);
+        // } else {
+        console.log("No data added");
+        // }
     } catch (err) {
         console.log("Error Occured", err);
     }
